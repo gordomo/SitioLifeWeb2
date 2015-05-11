725dfc3f41ce919f0702a9bb380800df42c2352b
Mori <morimartin@gmail.com>
2015-05-11 19:55:02 -0300

    Agrego guardar mail en el formulario de contacto

---
+++ conf/constantes.php
@@ -0,0 +1,5 @@
+<?php
+
+const OK = '0';
+const MENSAJE_OK = 'OK';
+const ERROR_AL_CONECTARSE = "1";
--- email.php
+++ email.php
@@ -1,32 +1,43 @@
 <?php
+include_once 'conf/conf.php';
 
-		// return "success"; die();
+    if($_REQUEST['first_name'] == '' || $_REQUEST['last_name'] == '' || $_REQUEST['email'] == '' || $_REQUEST['message'] == ''):
+        return "error";
+    endif;
 		
-		if($_REQUEST['first_name'] == '' || $_REQUEST['last_name'] == '' || $_REQUEST['email'] == '' || $_REQUEST['message'] == ''):
-			return "error";
-		endif;
-		
-		if (filter_var($_REQUEST['email'], FILTER_VALIDATE_EMAIL)):
-			// receiver email address
-			$to = 'receiver_email@domain.com';
+    if (filter_var($_REQUEST['email'], FILTER_VALIDATE_EMAIL)):
+    // receiver email address
+    $to = 'contacto@lifeweb.com';
 			
-			// prepare header
-			$header = 'From: '. $_REQUEST['first_name'] .' '. $_REQUEST['last_name']. ' <'. $_REQUEST['email'] .'>'. "\r\n";
-			$header .= 'Reply-To:  '. $_REQUEST['first_name'] .' '. $_REQUEST['last_name']. ' <'. $_REQUEST['email'] .'>'. "\r\n";
-			// $header .= 'Cc:  ' . 'example@domain.com' . "\r\n";
-			// $header .= 'Bcc:  ' . 'example@domain.com' . "\r\n";
-			$header .= 'X-Mailer: PHP/' . phpversion();
-			
-			// Contact Subject
-			$subject = $_REQUEST['subject'];
-			
-			// Contact Message
-			$message = $_REQUEST['message'];
-			
-			// Send contact information
-			$mail = mail( $to, $subject , $message, $header );
-			
-			return $mail ? "success" : "error";
-		else:
-			return "error";
-		endif;
+    // prepare header
+    $header = 'From: '. $_REQUEST['first_name'] .' '. $_REQUEST['last_name']. ' <'. $_REQUEST['email'] .'>'. "\r\n";
+    $header .= 'Reply-To:  '. $_REQUEST['first_name'] .' '. $_REQUEST['last_name']. ' <'. $_REQUEST['email'] .'>'. "\r\n";
+    // $header .= 'Cc:  ' . 'example@domain.com' . "\r\n";
+    // $header .= 'Bcc:  ' . 'example@domain.com' . "\r\n";
+    $header .= 'X-Mailer: PHP/' . phpversion();
+    // Contact Subject
+    $subject = $_REQUEST['subject'];
+    // Contact Message
+    $message = $_REQUEST['message'];
+    // Send contact information
+    $mail = mail( $to, $subject , $message, $header );
+    
+    if($mail)
+    {
+        $con = conf::getConection("m2000364_pruebas");
+        if($con)
+        {
+            mysqli_query($con,"INSERT INTO email (mail, nombre, apellido) "
+                                        . "VALUES ('".$_REQUEST['email']."','".$_REQUEST['first_name']."','".$_REQUEST['last_name']."')");
+            mysqli_close($con);
+        }
+    
+        return "success";
+    }
+    return "error";
+    else:
+        return "error";
+    endif;
+
+
+
